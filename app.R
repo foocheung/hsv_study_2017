af7994d20db2773e98c2bf675c4d0cad5943619f
foocheung <foo.cheung@nih.gov>
2023-01-06 12:42:28 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 ## Shiny R Package for batching Samples
 ## Created using the OSAT and Golem R packages
-## https://github.com/foocheung/batchingsamples
+## R package code base can be found here: https://github.com/foocheung/batchingsamples
 
 library(batchingsamples)
 library(shiny)
